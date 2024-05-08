cbd88c0e57c76e812a716914bd1f618c12dc4dcd
-1 <-1@HOME-PC>
2024-05-08 21:28:21 +0300

    Добавьте файлы проекта.

---
+++ ASP_LAB4/Books.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ASP_LAB4;
+
+namespace ASP_LAB4
+{
+        public class Books
+        {
+            public List<string> BookList { get; set; } = new List<string>();
+        }
+}
---
+++ ASP_LAB4/Profile.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP_LAB4
+{
+
+        public class Profile
+        {
+            public string Name { get; set; } = string.Empty;
+            public string Email { get; set; } = string.Empty;
+        }
+}
